5d9549d89a62f753baf8cfe917adc60987e8181e
Viktoriia <vaivanovava@gmail.com>
2023-01-11 12:05:47 +0300

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,23 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean2.getMessage());
+        Cat cat =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(cat.getMessage());
+        Cat cat2 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(cat2.getMessage());
+        System.out.println("Переменные Helloworld ссылаются на один объект? " + (bean == bean2));
+        System.out.println("Переменные Cat ссылаются на один объект? " + (cat == cat2));
+    }
+}
